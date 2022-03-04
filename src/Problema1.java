029cb7a9678b33e55e6d40a1d18c05c2ca7f3e30
Sergio Suria Mora <sergio.suriamora@iesfm.onmicrosoft.com>
2022-03-04 09:51:23 +0100

    Problema 1

---
+++ src/Problema1.java
@@ -0,0 +1,18 @@
+public class Problema1 {
+
+    public int Problema1(){
+        String fizz = "fizz";
+        String buzz = "buzz";
+        String n = null;
+        for (int i = 0; i < 20; i++) {
+
+            
+        }
+
+        return 0;
+    }
+}
+
+
+
+
---
+++ test/TestsProblema1.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestsProblema1 {
+
+    @Test
+    public void Problema1Test(){
+        Problema1 problema1 = new Problema1();
+        Assert.assertEquals("1234567891011121314151617181920", problema1);
+
+
+
+
+        }
+
+
+    }
+
